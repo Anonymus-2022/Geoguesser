5c79102ed7a93b5256638f945fbb1f79fa008b67
fspreng <fspreng@techfak.uni-bielefeld.de>
2023-10-24 18:03:28 +0200

    die ersten logging-funktionen sind implementiert aberes werden momentan nur die Dateien/Directories geloggt, die in asserts liegen, die schleife zu tieferen ebenen muss noch gemacht werden.

--- app/src/main/java/com/gse23/fspreng/MainActivity.java
+++ app/src/main/java/com/gse23/fspreng/MainActivity.java
@@ -1,8 +1,11 @@
 package com.gse23.fspreng;
-
+import static java.nio.file.Files.list;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 
 /**
@@ -12,7 +15,26 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+            try {
+                String[] inAssets;
+                inAssets = getAssets().list("albums");
+                int i = 0;
+                while (true){
+                    while (i < inAssets.length) {
+                        if (inAssets[i] != null) {
+                            Log.i("/albums/...", "Beinhält: " + inAssets[i]);
+                            i++;
+                        }
+                    }
+                break;
+                }
+                Log.i("marker", "keine weiteren files / directories");
+            }catch(IOException e) {
+                Log.e("ERROR", "Ordner /assets ist leer");
+            }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
