4444772af0f96655e619f93198f913a2e7744463
fspreng <fspreng@techfak.uni-bielefeld.de>
2023-10-25 15:57:09 +0200

    checkstyle korrigiert

--- app/src/main/java/com/gse23/fspreng/MainActivity.java
+++ app/src/main/java/com/gse23/fspreng/MainActivity.java
@@ -1,12 +1,9 @@
 package com.gse23.fspreng;
-import static java.nio.file.Files.list;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import java.io.IOException;
-import java.nio.file.Paths;
-
 
 /**
  * Das ist die MainActivity, die zuerst ausgeführt wird, wenn die App gestartet wird.
@@ -15,19 +12,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String answer = "Enthält: ";
         try {
             String[] inAssets = getAssets().list("albums");
             for (int i = 0; i < inAssets.length; i++) {
                 if (inAssets[i] != null) {
-                    Log.i("/albums...", "Beinhält: " + inAssets[i]);
-                    String[] a = getAssets().list("albums/" + inAssets[i]);
-                    for (int j = 0; j < a.length; j++) {
+                    Log.i("/albums", answer + inAssets[i]);
+                    String[] next = getAssets().list("albums/" + inAssets[i]);
+                    for (int j = 0; j < next.length; j++) {
                         boolean x = false;
-                        if (a[j] != null) {
-                            Log.i("/albums/" + inAssets[i], "Beinhält: " + a[j]);
-                            String[] subfolders = getAssets().list("albums/" + inAssets[i] + "/" + a[j]);
-                        }else{
-                            Log.i("albums/"+inAssets[i], "ist Leer");
+                        if (next[j] != null) {
+                            Log.i("/albums/" + inAssets[i], answer + next[j]);
+                            String[] subfolders = getAssets().list("albums/" + inAssets[i] + "/" + next[j]);
+                        } else {
+                            Log.i("albums/" + inAssets[i], "ist Leer");
                         }
                     }
                 }
