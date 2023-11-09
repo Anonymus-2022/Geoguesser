383c6a4ff653de9e2ba203fbddffc02c4bf64d0b
fspreng <fspreng@techfak.uni-bielefeld.de>
2023-11-09 14:02:41 +0100

    checsrtyle

--- app/src/main/java/com/gse23/fspreng/exception/CorruptedExifDataException.java
+++
@@ -1,6 +0,0 @@
-package com.gse23.fspreng.exception;
-
-public class CorruptedExifDataException extends Exception{
-
-
-}
--- app/src/main/java/com/gse23/fspreng/exception/EmptyAlbumException.java
+++
@@ -1,4 +0,0 @@
-package com.gse23.fspreng.exception;
-
-public class EmptyAlbumException {
-}
