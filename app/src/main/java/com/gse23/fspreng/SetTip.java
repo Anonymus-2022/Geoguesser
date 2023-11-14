d26d982f909e3a5df12954291ca0222550d0b076
fspreng <fspreng@techfak.uni-bielefeld.de>
2023-11-14 17:53:29 +0100

    Ich habe nun eine Möglichkeit, für eine Eingabe der Schätzung des users. Diese eingabe ist in einer Variablen gespeichert und wird in der Logcat ausgegeben.

--- app/src/main/java/com/gse23/fspreng/SetTip.java
+++ app/src/main/java/com/gse23/fspreng/SetTip.java
@@ -1,21 +1,71 @@
 package com.gse23.fspreng;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
- * Hier wird dem Spieler ermöglicht, die Position des gesehehenen Bildes zu schätzen.
+ * Hier wird dem Spieler ermöglicht, die Position des gesehenen Bildes zu schätzen.
  */
 public class SetTip extends AppCompatActivity {
 
-    protected void onCreate(Bundle savedBundleInstances) {
-
-        super.onCreate(savedBundleInstances);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.set_tip);
+
         Button back = findViewById(R.id.button);
+        EditText latitudeIn = findViewById(R.id.latitude);
+        EditText longitudeIn = findViewById(R.id.longitude);
+        Button confirm = findViewById(R.id.confirm);
+
+
+        // Event Handling für die Breiten-EditText
+        latitudeIn.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String latitudeStr = editable.toString();
+                Log.i("Input Coordinate", "Latitude: " + latitudeStr);
+            }
+        });
+
+        longitudeIn.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String latitudeStr = editable.toString();
+                Log.i("Input Coordinate", "Latitude: " + latitudeStr);
+            }
+        });
+
+        confirm.setOnClickListener(v -> {
+            // Hier kannst du den eingegebenen Breiten- und Längengrad verarbeiten
+            String latitudeStr = latitudeIn.getText().toString();
+            String longitudeStr = longitudeIn.getText().toString();
+            Log.i("Entered Coordinates", "Latitude: " + latitudeStr + ", Longitude: " + longitudeStr);
+        });
 
         back.setOnClickListener(v -> finish());
+
     }
 }
